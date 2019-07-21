c43659e7c267198614b9bfb58dbf0019a1c6686f
Peter Lejeck <peter.lejeck@gmail.com>
2019-07-21 16:02:55 -0700

    Add our primary Strait interface

--- lib/strait.rb
+++ lib/strait.rb
@@ -3,18 +3,39 @@
 require 'strait/version'
 require 'strait/dsl'
 require 'strait/configuration'
+require 'strait/rule'
 
-# A rate-limiter which provides defense for your nation-state. Or your API.
+# A rate-limiter which provides natural defenses for your nation-state. Or your API.
 class Strait
-  attr_reader :name
+  class RateLimitExceeded < StandardError; end
 
-  def initialize(name, limits = [])
+  attr_reader :name, :config
+
+  def initialize(name, rules: [], **config)
     @name = name
-    @limits = limits
-    Strait::DSL.new(@limits, &Proc.new) if block_given?
+    @raw_rules = rules
+    @config = Strait::Configuration.default.merge(config)
+
+    @raw_rules += Strait::DSL.new(&Proc.new).rules if block_given?
+  end
+
+  def limit!(user)
+    rules.each { |rule| rule.call(user) }
   end
 
   def self.configuration=(config)
     Strait::Configuration.default = Strait::Configuration.new(config)
+  end
+
+  private
+
+  def rules
+    @rules ||= @raw_rules.map do |rule|
+      if rule.is_a?(Strait::Rule)
+        rule
+      else
+        Strait::Rule.new(config: @config, name: @name, rule: rule)
+      end
+    end
   end
 end
