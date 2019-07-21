1319361aadbce932aaa05aa6d8437aa8c6d0f1ea
Peter Lejeck <peter.lejeck@gmail.com>
2019-07-21 14:57:34 -0700

    Add simplecov for testing coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,17 @@
-require "bundler/setup"
-require "strait"
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+  track_files 'lib/**/*.rb'
+end
+
+require 'strait'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
